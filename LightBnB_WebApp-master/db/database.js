de5f78e92a2b3ea4b2d01591e41b1e0f85c86991
carolineolagunju <olamidejr21@gmail.com>
2023-08-08 17:51:20 -0600

    implement function getAllReservations

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -53,6 +53,9 @@ const getUserWithId = function(id) {
       return null;
     }
     return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
   });
 }
 
@@ -69,6 +72,9 @@ const addUser = function(user) {
   .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
   .then((result) => {
     return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
   });
 }
 
@@ -79,9 +85,29 @@ const addUser = function(user) {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
+// const getAllReservations = function (guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// };
+
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2`, [guest_id, limit])
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
+
+
 
 /// Properties
 
@@ -98,7 +124,6 @@ const getAllProperties = (options, limit = 10) => {
   return pool
   .query(`SELECT * FROM properties LIMIT $1`, [limit])
   .then((result) => {
-    console.log(result.rows);
     return result.rows;
   })
   .catch((err) => {
