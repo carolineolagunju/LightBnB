d8f7c3561b9de1fcf8e2b2a2f25baa1261d5439a
carolineolagunju <olamidejr21@gmail.com>
2023-08-09 17:16:52 -0600

    clean code

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -166,7 +166,7 @@ const getAllProperties = (options, limit = 10) => {
   LIMIT $${queryParams.length};
   `;
 
-    // Execute the query using the database connection pool and return the result rows
+  // Execute the query using the database connection pool and return the result rows
   return pool.query(queryString, queryParams).then((res) => {
     return res.rows;
   });
@@ -179,16 +179,10 @@ const getAllProperties = (options, limit = 10) => {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-// const addProperty = function(property) {
-//   const propertyId = Object.keys(properties).length + 1;
-//   property.id = propertyId;
-//   properties[propertyId] = property;
-//   return Promise.resolve(property);
-// };
 
 const addProperty = function(property) {
-return pool
-.query(`INSERT INTO properties (
+  return pool
+    .query(`INSERT INTO properties (
   owner_id,
   title,
   description,
@@ -205,13 +199,13 @@ return pool
   number_of_bedrooms)
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
   RETURNING *`,
-  [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
-  .then(result => {
-    return result.rows[0];
-  })
-  .catch(err => {
-    console.log(err);
-  })
+    [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+    .then(result => {
+      return result.rows[0];
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 
